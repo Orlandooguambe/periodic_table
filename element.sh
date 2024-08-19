1900728e1cd7849340acca6a40cfb5a11a770365
Orlandooguambe Guambe <orlandooguambe@gmail.com>
2024-08-19 22:33:04 +0000

    feat: Add symbol

--- element.sh
+++ element.sh
@@ -6,7 +6,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 then
   echo "Please provide an element as an argument."
 else
- # if [[ $1 =~ ^[0-9]+$ ]]
+ if [[ $1 =~ ^[0-9]+$ ]]
   then
     ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
   else
