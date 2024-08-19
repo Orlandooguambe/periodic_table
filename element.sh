d377cf886535bca495c18eee5ba5aad3fd25986e
Orlandooguambe Guambe <orlandooguambe@gmail.com>
2024-08-19 22:55:31 +0000

    fix: only output needed lines

--- element.sh
+++ element.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
+# Connect to the PostgreSQL database
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-[[ -z $1 ]]
+# Check if an argument was provided
+if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
- if [[ $1 =~ ^[0-9]+$ ]]
+  # Determine if the input is an atomic number, symbol, or name
+  if [[ $1 =~ ^[0-9]+$ ]]
   then
-    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+    # Input is an atomic number
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+                     FROM elements 
+                     INNER JOIN properties USING(atomic_number) 
+                     INNER JOIN types USING(type_id) 
+                     WHERE atomic_number=$1")
+  elif [[ $1 =~ ^[a-zA-Z]{1,2}$ ]]
+  then
+    # Input is an element symbol
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+                     FROM elements 
+                     INNER JOIN properties USING(atomic_number) 
+                     INNER JOIN types USING(type_id) 
+                     WHERE symbol='$1'")
   else
-    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+    # Input is an element name
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+                     FROM elements 
+                     INNER JOIN properties USING(atomic_number) 
+                     INNER JOIN types USING(type_id) 
+                     WHERE name='$1'")
   fi
 
+  # Check if the element was found
   if [[ -z $ELEMENT ]]
   then
     echo "I could not find that element in the database."
   else
-    echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
-    do
-      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-    done
+    # Parse the results and display the information
+    IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< $ELEMENT
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   fi
 fi
-
-#git branch -m master main
-
-#git remote set-url origin https://github.com/Orlandooguambe/periodic_table.git
-#git push -u origin main
-
